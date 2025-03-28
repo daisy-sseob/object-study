68d45ca6b9fc2ae4c33cbce51f113438d8bec45b
daisy-sseob <orolsyeo@gmail.com>
2025-03-28 12:35:18 +0900

    init

---
+++ src/test/java/com/sseob/javaobjectstudy/JavaObjectStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sseob.javaobjectstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaObjectStudyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
